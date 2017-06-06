b6a574656631f6726672e9b2944392880e26a464
Phil Lopreiato <plnyyanks@gmail.com>
2017-06-05 21:39:20 -0400

    Image tagging

--- build-images.sh
+++ build-images.sh
@@ -4,8 +4,9 @@ set -e
 # For each directory, push each Dockerfile
 for dir in $(ls -d */); do
     cd $dir
+    IMAGE=${dir:0:-1}
     REPO=the-blue-alliance/${dir:0:-1}
     echo "Building $REPO..."
-    docker build -f Dockerfile -t $REPO:$COMMIT .
+    docker build -f Dockerfile -t $IMAGE .
     cd ..
 done
